eb59eb21d66efd62e70635d191fdf8dd65f12267
Dany <danymike.joseph@gmail.com>
2022-04-28 19:25:06 +0200

    feat: payment endpoint with mockOrderProduct done

--- monolith/controllers/Payment.js
+++ monolith/controllers/Payment.js
@@ -1,61 +1,76 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { Payment, Notification, MockOrderProduct } = require("../models");
 
-const calculateOrderAmount = (items) => {
-    // Retrieve order_id and fetch all items
-    let totalPrice = 0;
-    items.forEach(item => {
-        totalPrice += item.price;
-    })
+const findOrderById = async function (id, res) {
+    const order = await MockOrderProduct.findOne({
+        where: {
+            id
+        }
+    });
+    if (order === null) {
+        res.status(400).send(`Order with id: ${id} not found`)
+    }
+    return order.dataValues;
+};
 
+const calculateOrderAmount = async (id, res) => {
+    const order = await findOrderById(id, res)
+    const qty = order.quantity
+    const unitPrice = order.unitPrice
+    const totalPrice = unitPrice * qty
     return totalPrice;
 };
 
 const setPaymentStatus = async (event) => {
-    const payment = await Payment.create({status: event.data.object.status});
+    const payment = await Payment.create({ status: event.data.object.status });
     await payment.save()
 }
 
 module.exports = {
     createPaymentIntent: async (req, res) => {
-        const { items } = req.body;
+        const { id } = req.body;
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: calculateOrderAmount(items),
-            currency: "eur",
-            automatic_payment_methods: {
-                enabled: true,
-            },
-        });
-        res.send({
-            clientSecret: paymentIntent.client_secret,
-        });
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: await calculateOrderAmount(id, res),
+                currency: "eur",
+                automatic_payment_methods: {
+                    enabled: true,
+                },
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+                totalPrice: paymentIntent.amount
+            });
+        } catch (err) {
+            console.log(err)
+            res.status(400).send(`${err}`)
+        }
+        
     },
     handleEvent: async (req, res) => {
 
         const sig = req.headers['stripe-signature'];
         let event;
-      
+
         try {
-          event = stripe.webhooks.constructEvent(req.rawBody, sig, process.env.WEBHOOK_SECRET);
-          const notification = await Notification.create({body: event});
-          await notification.save()
-          const orderProduct = await MockOrderProduct.create({unitPrice: 1000, quantity: 3})
-          await orderProduct.save()
+            event = stripe.webhooks.constructEvent(req.rawBody, sig, process.env.WEBHOOK_SECRET);
+            const notification = await Notification.create({ body: event });
+            await notification.save()
         } catch (err) {
-          res.status(400).send(`Webhook Error: ${err.message}`);
-          console.log(err.message);
-          return;
+            res.status(400).send(`Webhook Error: ${err.message}`);
+            console.log(err.message);
+            return;
         }
 
         switch (event.type) {
             case 'payment_intent.canceled':
                 setPaymentStatus(event);
                 return res.status(200)
-              case 'payment_intent.payment_failed':
+            case 'payment_intent.payment_failed':
                 setPaymentStatus(event);
                 return res.status(200)
-              default:
+            default:
                 console.log(`Unhandled event type ${event.type}`);
         }
         res.json({ received: true });
