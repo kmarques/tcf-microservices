1ffbd525e39f5ed83694bbfd05fc01ae19b106f8
Maroua Bihich <bihich.maroua@gmail.com>
2022-05-10 08:50:19 +0200

    creation du microservices inventory

---
+++ microservices/inventory/index.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");
+[".env", ".env.local"].forEach((path) => dotenv.config({ path }));
+
+const express = require("express");
+const app = express();
+
+const InventoryController = require("./controllers/Inventory");
+
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
+
+
+ app.use(
+  "/",
+  require("./routes/Inventory")
+ );
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port " + process.env.PORT);
+});
+
+// setInterval(() => {
+//   InventoryController.removeExpired();
+// }, 5 * 1000);
