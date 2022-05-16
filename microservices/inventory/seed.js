9bdb84363feb38d37b68abfce246caae8d7016ab
Maroua Bihich <bihich.maroua@gmail.com>
2022-05-16 16:45:49 +0200

    creation de la bdd

--- microservices/inventory/seed.js
+++ microservices/inventory/seed.js
@@ -4,21 +4,21 @@ const { Inventory } = require("./models");
 (async () => {
   await Inventory.create({
     orderId: 1,
-    productId: 12,
+    ProductId: 12,
     typeEvent: "ADD",
     quantity: 12,
 
     
   }), await Inventory.create({
     orderId: 2,
-    productId: 11,
+    ProductId: 11,
     typeEvent: "PURCHASE",
     quantity: 2,
 
     
   }), await Inventory.create({
     orderId: 3,
-    productId: 11,
+    ProductId: 11,
     typeEvent: "PURCHASE",
     quantity: 2,
 
