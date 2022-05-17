db1a6cbf25b718e6d3ed6ee0ab54001a22676f5c
Arthur Isnard <artisnard@gmail.com>
2022-05-17 18:08:27 +0200

    wip: funcional tests for authentication service

--- microservices/authentication/__test__/security.test.js
+++ microservices/authentication/__test__/security.test.js
@@ -1,5 +1,6 @@
 const supertest = require("supertest");
 const sequelize = require("../models/index").sequelize;
+const userFixture = require("../fixtures/user.js");
 
 const client = supertest(require("../app.js"));
 
@@ -21,10 +22,10 @@ describe("Security", () => {
   it("creates a user and returns it in the body of the response", async () => {
     // Given
     const givenUser = {
-      lastname: "hey",
-      firstname: "hey",
-      email: "hey@hey.com",
-      password: "hey",
+      lastname: "testing",
+      firstname: "testing",
+      email: `${Date.now()}@testing.com`,
+      password: "testing",
     };
     // When
     const response = await client.post("/register").send(givenUser);
@@ -38,9 +39,10 @@ describe("Security", () => {
 
   it("returns a bearer token if the user exists", async () => {
     // Given
+    const { user } = await userFixture();
     const givenUser = {
-      email: "bi@bi.com",
-      password: "bi",
+      email: user.email,
+      password: "testing",
     };
     // When
     const response = await client.post("/login").send(givenUser);
@@ -51,17 +53,18 @@ describe("Security", () => {
 
   it("returns status code 200 and the user email if the bearer token is valid", async () => {
     // Given
+    const { user } = await userFixture();
     const givenUser = {
-      email: "bi@bi.com",
-      password: "bi",
+      email: user.email,
+      password: "testing",
     };
-    const givenBearer = await (
-      await client.post("/login").send(givenUser)
-    ).body.token;
+    const givenConnection = await client.post("/login").send(givenUser);
+    console.log(givenConnection.body);
+    console.log("GivenConnection", givenConnection.body);
     // When
     const response = await client
       .post("/verify-token")
-      .set("Authorization", `Bearer ${givenBearer}`);
+      .set("Authorization", `Bearer ${givenConnection.body.token}`);
     // Then
     expect(response.status).toBe(200);
     expect(response.body.email).toBe(givenUser.email);
